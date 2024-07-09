7497c8ab09db8c59cec2a9fd04be171bb19951ec
Pong <pongpong34756@gmail.com>
2024-07-09 11:02:40 +0700

    feat: add landing page mobile responsive

---
+++ client/src/pages/landingpage.jsx
@@ -0,0 +1,16 @@
+import Hero from "../components/homepage/hero";
+import Service from "../components/homepage/service";
+import Contact from "../components/homepage/contact";
+import Footer from "../components/homepage/footer";
+function Homepage() {
+  return (
+    <div>
+      <Hero />
+      <Service />
+      <Contact />
+      <Footer />
+    </div>
+  );
+}
+
+export default Homepage;
