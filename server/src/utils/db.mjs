0e23c5d3fb1457c2188152a32edbba607fb4ba1e
trainthunder <kitikun.po@gmail.com>
2024-07-12 17:14:49 +0700

    feat: build admin-login api

--- server/src/utils/db.mjs
+++ server/src/utils/db.mjs
@@ -1,10 +1,10 @@
 // Create PostgreSQL Connection Pool here !
-import * as pg from 'pg';
+import * as pg from "pg";
 const { Pool } = pg.default;
-import 'dotenv/config';
+import "dotenv/config";
 
 const connectionPool = new Pool({
-  connectionString: process.env.DB_URL
+  connectionString: process.env.DB_URL,
 });
 
 export default connectionPool;
