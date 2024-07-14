6fe1080f112608cd9f7210c6a515dfb0457deec3
trainthunder <kitikun.po@gmail.com>
2024-07-14 23:22:13 +0700

    test: build button logout in admin-home-page for admin

--- client/src/pages/admin-home-page.jsx
+++ client/src/pages/admin-home-page.jsx
@@ -1,27 +1,27 @@
 import React from "react";
 import { useAuth } from "../context/authentication";
+import logoutIcon from "/icons/logout-icon.png";
 import axios from "axios";
 
 function AdminHomePage() {
   const { adminLogout } = useAuth();
   return (
-    <div className="w-screen h-screen bg-blue-300">
-      <p className="text-[60px] font-prompt text-black">คุณมาถึงหน้าHomeแล้ว</p>
-      <button
-        className="w-[200px] h-[100px] bg-gray-400 text-[50px] text-black"
-        onClick={() => adminLogout()}
-      >
-        Logout
-      </button>
-
-      <button
-        className="w-[200px] h-[50px] bg-white"
-        onClick={async () => {
-          await axios.get("http://localhost:4000/");
-        }}
-      >
-        get
-      </button>
+    <div className="w-screen h-screen bg-background">
+      <section className="w-[240px] h-screen bg-blue-950 flex flex-col">
+        <header className="w-full h-[106px] bg-green-300"></header>
+        <section className="h-full flex flex-col justify-between">
+          <div className="w-full h-[170px] bg-red"></div>
+          <button
+            className="w-full h-[54px] mb-[50px] flex items-center justify-center"
+            onClick={() => adminLogout()}
+          >
+            <img src={logoutIcon} alt="" />
+            <p className="text-[16px] font-prompt text-white ml-[16px]">
+              ออกจากระบบ
+            </p>
+          </button>
+        </section>
+      </section>
     </div>
   );
 }
