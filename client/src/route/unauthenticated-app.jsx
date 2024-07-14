fe95adcd38aa7e70c20eeef8944c88eb2729e0d2
Pong <pongpong34756@gmail.com>
2024-07-14 17:00:07 +0700

    fix: update unauthenticated route

--- client/src/route/unauthenticated-app.jsx
+++ client/src/route/unauthenticated-app.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import AdminLoginPage from "../pages/admin-login-page";
+import Homepage from "../pages/landing-page";
+import Servicelist from "../pages/service-list-page";
 
 function UnauthenticatedApp() {
   return (
     <div className="App">
       <Routes>
         <Route path="/admin/login" element={<AdminLoginPage />} />
+        <Route path="/" element={<Homepage />} />
+        <Route path="/servicelist" element={<Servicelist />} />
       </Routes>
     </div>
   );
