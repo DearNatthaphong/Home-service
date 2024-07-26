c284239c26fc4306a121f00175969ec91f949fb9
trainthunder <kitikun.po@gmail.com>
2024-07-26 14:17:57 +0700

    update: change image path name on admin login page

--- client/src/pages/admin-login-page.jsx
+++ client/src/pages/admin-login-page.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 import AdminLoginForm from "../components/admin-login-page/admin-login-form";
-import houseIcon from "/icons/house-icon.png";
 
 function AdminLoginPage() {
   return (
     <div className="w-screen h-screen bg-background flex items-center justify-center flex-col gap-[45px]">
       <div className="flex">
-        <img src={houseIcon} alt="" />
+        <img src="/icons/house-icon.png" alt="" />
         <p className="font-prompt text-blue-600 text-[52px] font-medium">
           HomeServices
         </p>
