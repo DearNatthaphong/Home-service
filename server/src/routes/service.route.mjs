fe692f409738bec32087df9ae6eed4c44c9fdbce
trainthunder <kitikun.po@gmail.com>
2024-08-12 12:55:23 +0700

    feat: get-three-service api

--- server/src/routes/service.route.mjs
+++ server/src/routes/service.route.mjs
@@ -1,9 +1,10 @@
-import { Router } from 'express';
-import * as serviceController from '../controllers/service.controller.mjs';
-import { protect } from '../middlewares/protect.middleware.mjs';
+import { Router } from "express";
+import * as serviceController from "../controllers/service.controller.mjs";
+import { protect } from "../middlewares/protect.middleware.mjs";
 
 const serviceRouter = Router();
 
-serviceRouter.get('/', serviceController.getAllService);
+serviceRouter.get("/", serviceController.getAllService);
+serviceRouter.get("/limit3", serviceController.getSomeService);
 
 export default serviceRouter;
